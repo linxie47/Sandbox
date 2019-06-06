0e6de4ca96384f472853531bfe68b33f61c6d57d
Lin Xie <lin.xie@intel.com>
2019-06-06 17:10:07 +0800

    Revert "C api v2 (#1)"
    This reverts commit 92654832d8aaaf87d61cd52aeebe75be5c1e7905.

--- openvino-c_api_v2.0/ie_wrapper/proc_factory/proc_factory.cpp
+++
@@ -1,8 +0,0 @@
-#include "proc_factory.h"
-
-PostProcFunction getPostProcFunctionByName(const char *name)
-{
-    if (!strcmp(name, "ie_detect"))
-        return (PostProcFunction)ExtractBoundingBoxes;
-    return nullptr;
-}
